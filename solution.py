dcdba49a7439f3df289f80e37a1ba576ce29a5cb
soycode <soycode@users.noreply.github.com>
2018-07-12 08:02:12 -0700

    Add some useful imports

--- solution.py
+++ solution.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+
 def contains_banana(img):
 	"""
 	Change the contents of this function so it behaves correctly
