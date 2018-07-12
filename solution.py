83ad4d35c92ce64050278e71ec1adc378638ea23
MarynaLongnickel <37321032+MarynaLongnickel@users.noreply.github.com>
2018-07-12 12:56:28 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.backend import tf as ktf
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 
 
 def contains_banana(img):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
-	return -3.14159
+    datagen = ImageDataGenerator()
+
+    img = load_img('C:/users/solar/Desktop/' + img)
+    img = ktf.image.resize_images(img, (224, 224))
+    img_array = img_to_array(img)
+    print(img_array.shape)
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    ax.axis('off')
+    plt.show()
+    return img
+
 
 def crop_image(img, quadrant):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
-	return img
+    l = img
+    return img
+
 
 def find_banana(img):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
-	return "None"
+    return "None"
+
+contains_banana('example0.jpeg')
