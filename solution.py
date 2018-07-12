8320b3d6def96ec917def9362fbd66cc36428826
Jason fleischer <jrjflei@gmail.com>
2018-07-12 10:03:22 -0600

    finished part 1

--- solution.py
+++ solution.py
@@ -6,10 +6,21 @@ from keras.applications.resnet50 import preprocess_input, decode_predictions
 
 
 def contains_banana(img):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
-	return -3.14159
+    model = ResNet50(include_top=True, 
+                     weights='imagenet', 
+                     input_tensor=None, 
+                     input_shape=None, 
+                     pooling=None, 
+                     classes=1000)
+    img = image.load_img(img, target_size=(224, 224))
+    img = image.img_to_array(img)
+    x = preprocess_input(np.expand_dims(img.copy(), axis=0))
+    preds = model.predict(x)
+    probs = decode_predictions(preds, top=3)
+    if 'banana' in probs:
+        return probs
+    else:     
+        return 0
 
 def crop_image(img, quadrant):
 	"""
