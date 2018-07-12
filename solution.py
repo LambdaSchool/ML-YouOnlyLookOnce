41a0e44b78c57ee1ba887c925b919efd5d07a657
astrebeck <alexstrebeck@gmail.com>
2018-07-12 09:58:57 -0600

    have they initial banana detector working

--- solution.py
+++ solution.py
@@ -6,26 +6,33 @@ from keras.applications.resnet50 import preprocess_input, decode_predictions
 
 model = ResNet50(weights='imagenet')
 
-neg =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/negative_examples"
-pos =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/postive_examples"
+neg =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/negative_examples/"
+pos =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/positive_examples/"
 
-negative_ex = [neg + 'example10',neg + 'example11',neg + 'example12']
+negative_ex = [neg + 'example10.jpeg',neg + 'example11.jpeg',neg + 'example12.jpeg']
 
-postive_ex = [pos + 'example0',pos + 'example1',pos + 'example2']
+positive_ex = [pos + 'example0.jpeg',pos + 'example1.jpeg',pos + 'example2.jpeg']
 
 def contains_banana(img):
 
-	preds = model.predict(img)
+	img = image.load_img(img, target_size=(224, 224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	x = preprocess_input(x)
 
-	print(preds)
+	preds = model.predict(x)
 
-	return -3.14159
+	results = decode_predictions(preds, top=3)[0]
+	results = [ res[1] for res in results]
+	# print(results)
 
-def crop_image(img, quadrant):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
-	return img
+	if( 'banana' in results ): return 1
+	else: return 0
+
+def crop_image(img_path, quadrant):
+
+	img = image.load_img(img_path, target_size=(2000,1500))
+
 
 def find_banana(img):
 	"""
@@ -35,4 +42,4 @@ def find_banana(img):
 
 
 
-contains_banana(negative_ex[0])
+print(contains_banana(positive_ex[0]))
