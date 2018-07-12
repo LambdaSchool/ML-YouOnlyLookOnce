0cd13ad25c01e83e325aeb134dde644cf0cc6268
astrebeck <alexstrebeck@gmail.com>
2018-07-12 09:32:56 -0600

    intial commit

--- solution.py
+++ solution.py
@@ -4,11 +4,21 @@ from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 
+model = ResNet50(weights='imagenet')
+
+neg =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/negative_examples"
+pos =  "C:/Users/Smartsrc-alex/Documents/ML_class/ML-YouOnlyLookOnce/sample_data/postive_examples"
+
+negative_ex = [neg + 'example10',neg + 'example11',neg + 'example12']
+
+postive_ex = [pos + 'example0',pos + 'example1',pos + 'example2']
 
 def contains_banana(img):
-	"""
-	Change the contents of this function so it behaves correctly
-	"""
+
+	preds = model.predict(img)
+
+	print(preds)
+
 	return -3.14159
 
 def crop_image(img, quadrant):
@@ -22,3 +32,7 @@ def find_banana(img):
 	Change the contents of this function so it behaves correctly
 	"""
 	return "None"
+
+
+
+contains_banana(negative_ex[0])
